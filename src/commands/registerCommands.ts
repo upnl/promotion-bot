984fb2c3b45ac633d606b2977c9ebc61b0d4c0e1
coinmoles <andrewpark421@gmail.com>
2023-12-21 18:13:24 +0900

    Made commands guild command when testing

--- src/commands/registerCommands.ts
+++ src/commands/registerCommands.ts
@@ -1,16 +1,21 @@
 import { Client, REST, Routes } from "discord.js";
 import { commands } from "./commands.js";
+import { isTesting } from "../isTesting.js";
 
 export const registerCommands = async (client: Client) => {
     const rest = new REST().setToken(process.env.TOKEN!);
 
     try {
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID!),
-            {
-                body: commands.map(command => command.builder)
-            }
-        )
+        if (isTesting)
+            await rest.put(
+                Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
+                { body: commands.map(command => command.builder) }
+            )
+        else
+            await rest.put(
+                Routes.applicationCommands(process.env.CLIENT_ID!),
+                { body: commands.map(command => command.builder) }
+            )
     }
     catch (error) {
         console.log(`There was an error: ${error}`);
