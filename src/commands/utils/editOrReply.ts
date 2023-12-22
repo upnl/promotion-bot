b3f8d044ad17a23be8aa43a514dc522b4975686e
coinmoles <andrewpark421@gmail.com>
2023-12-22 17:52:19 +0900

    Fixed bug: Some responses caused crash

--- src/commands/utils/editOrReply.ts
+++ src/commands/utils/editOrReply.ts
@@ -1,7 +1,7 @@
 import { InteractionReplyOptions, RepliableInteraction } from "discord.js";
 
 export const editOrReply = async (interaction: RepliableInteraction, option: InteractionReplyOptions) => {
-    if (interaction.replied)
+    if (interaction.replied || interaction.deferred)
         return await interaction.editReply(option)
     else
         return await interaction.reply(option)
